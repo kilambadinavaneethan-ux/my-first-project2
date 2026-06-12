b5a43ac92e415d5744fdaf7a23bad626337cbca6
kilambadinavaneethan-ux <kilambadinavaneethan@gmail.com>
2026-06-12 13:33:21 +0530

    Updated code

--- src/pages/Expenses.jsx
+++ src/pages/Expenses.jsx
@@ -2,7 +2,7 @@ function Expenses() {
   return (
     <section>
       <div className="dashboard-hero">
-        <h1>Expenses Tracker</h1>
+        <h1>fun Tracker</h1>
         <p>
           Track business spending, recurring costs, and one-time expenses. Use this
           page to monitor cash flow and keep your operating costs under control.
